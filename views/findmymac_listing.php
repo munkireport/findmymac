19eb5368ff22d2fdf64952423e7b23f222f26f1c
Arjen van Bochoven <nejra@me.com>
2019-07-08 17:27:53 +0200

    Remove model init from view

--- views/findmymac_listing.php
+++ views/findmymac_listing.php
@@ -1,11 +1,5 @@
 <?php $this->view('partials/head', array('scripts' => array('clients/client_list.js'))); ?>
 
-<?php //Initialize models needed for the table
-new Machine_model;
-new Reportdata_model;
-new findmymac_model;
-?>
-
 <div class="container">
 
   <div class="row">
