a49b2be0cf51d2b54ebf69c4be5f9ff0266ee261
Mosen <mosen@users.noreply.github.com>
2021-05-22 18:41:30 +1000

    Bootstrap 4 Style Updates for MunkiReport-PHP v6.0.0 beta 1 Start using version numbers in dependent packages.

--- views/findmymac_listing.php
+++ views/findmymac_listing.php
@@ -1,9 +1,9 @@
 <?php $this->view('partials/head', array('scripts' => array('clients/client_list.js'))); ?>
 
-<div class="container">
-    <div class="row">
+<div class="container-fluid">
+    <div class="row pt-4">
         <div class="col-lg-12">
-            <h3><span data-i18n="findmymac.listing.title"></span> <span id="total-count" class='label label-primary'>…</span></h3>
+            <h3><span data-i18n="findmymac.listing.title"></span> <span id="total-count" class='badge badge-primary'>…</span></h3>
             <table class="table table-striped table-condensed table-bordered">
                 <thead>
                     <tr>
